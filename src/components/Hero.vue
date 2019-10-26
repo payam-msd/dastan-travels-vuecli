a55b7b7605d4405d66e776a0c6140c3dcc9d8d39
payam <payammgh17@gmail.com>
2019-10-26 17:58:01 +0330

    Add: vue-awesome-swiper

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,173 +1,186 @@
 <template>
-  <div>
-    <div id="Hero">
-      <agile
-        ref="main"
-        class="main"
-        :options="options1"
-        :as-nav-for="asNavFor1"
-      >
-        <div
-          v-for="(slide, index) in slides"
-          :key="index"
-          class="slide"
-          :class="`slide--${index}`"
-        >
-          <img :src="slide" />
-        </div>
-      </agile>
-      <agile
-        ref="thumbnails"
-        class="thumbnails"
-        :options="options2"
-        :as-nav-for="asNavFor2"
-      >
-        <div
-          v-for="(slide, index) in slides"
-          :key="index"
-          class="slide slide--thumbniail"
-          :class="`slide--${index}`"
-          @click="$refs.thumbnails.goTo(index)"
-        >
-          <img :src="slide" />
-        </div>
-        <template slot="prevButton">
-          <icon
-            class=" text-white"
-            name="arrow-left-circle"
-            stroke-width="2"
-            height="28"
-            width="28"
-          />
-        </template>
-        <template slot="nextButton">
-          <icon
-            class=" text-white"
-            name="arrow-right-circle"
-            stroke-width="2"
-            height="28"
-            width="28"
-          />
-        </template>
-      </agile>
-    </div>
-  </div>
+	<md-card class="test">
+		<md-card-media style="height: 500px">
+			<!-- swiper1 -->
+			<swiper :options="swiperOptionTop" class="gallery-top" ref="swiperTop">
+				<swiper-slide class="`slide${i}`" v-for="(slide, i) in slides" :key="i">
+					<img class="rounded m-2 bg-center bg-cover" :src="slide" alt />
+				</swiper-slide>
+				<div class="swiper-button-next" slot="button-next">
+					<icon
+						class="text-white"
+						name="arrow-right-circle"
+						stroke-width="2"
+						height="28"
+						width="28"
+					/>
+				</div>
+				<div class="swiper-button-prev" slot="button-prev">
+					<icon
+						class="text-white"
+						name="arrow-left-circle"
+						stroke-width="2"
+						height="28"
+						width="28"
+					/>
+				</div>
+			</swiper>
+			<!-- swiper2 Thumbs -->
+			<swiper :options="swiperOptionThumbs" class="gallery-thumbs" ref="swiperThumbs">
+				<swiper-slide v-for="(slide, i) in slides" :key="i" class="slide-1">
+					<img class="rounded m-2 bg-center bg-cover" :src="slide" alt />
+				</swiper-slide>
+			</swiper>
+		</md-card-media>
+	</md-card>
+
+	<!-- <div class>
+		<icon
+			class="text-white"
+			name="arrow-left-circle"
+			stroke-width="2"
+			height="28"
+			width="28"
+		/>
+		<icon
+			class="text-white"
+			name="arrow-right-circle"
+			stroke-width="2"
+			height="28"
+			width="28"
+		/>
+	</div>-->
 </template>
 
 <script>
+import "swiper/dist/css/swiper.css"
+
+import {swiper, swiperSlide} from "vue-awesome-swiper"
 export default {
-  name: "Hero",
-  mounted() {
-    this.asNavFor1.push(this.$refs.thumbnails);
-    this.asNavFor2.push(this.$refs.main);
-  },
-  data() {
-    return {
-      asNavFor1: [],
-      asNavFor2: [],
-      options1: {
-        speed: 750,
-        dots: false,
-        fade: true,
-        navButtons: false
-      },
-
-      options2: {
-        autoplay: false,
-        centerMode: true,
-        speed: 750,
-
-        dots: false,
-        navButtons: false,
-        slidesToShow: 3,
-        responsive: [
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 5
-            }
-          },
-
-          {
-            breakpoint: 1000,
-            settings: {
-              navButtons: true
-            }
-          }
-        ]
-      },
-
-      slides: [
-        "https://res.cloudinary.com/dgkw9qspp/image/upload/v1571562370/client-img-10_1_asbbfb.webp",
-        // "https://res.cloudinary.com/dgkw9qspp/image/upload/v1571562312/client-img-9_1_magmx4.webp",
-        // "https://res.cloudinary.com/dgkw9qspp/image/upload/v1571562258/client-img-8_1_qoxzwt.webp",
-        "https://res.cloudinary.com/dgkw9qspp/image/upload/v1571561966/client-img-3_1_qhsxmz.webp",
-        "https://res.cloudinary.com/dgkw9qspp/image/upload/v1571562211/client-img-7_1_ik7x2x.webp",
-        "https://res.cloudinary.com/dgkw9qspp/image/upload/v1571562093/client-img-5_1_qimmco.webp",
-        "https://res.cloudinary.com/dgkw9qspp/image/upload/v1571561733/client-img-1_1_pk3ip6.webp",
-        "https://res.cloudinary.com/dgkw9qspp/image/upload/v1571561885/client-img-2_1_qencbx.webp",
-        // "https://res.cloudinary.com/dgkw9qspp/image/upload/v1571562031/client-img-4_1_k0zfzc.webp",
-        "https://res.cloudinary.com/dgkw9qspp/image/upload/v1571562143/client-img-6_1_idhztt.webp"
-      ]
-    };
-  }
-};
+	name: "Hero",
+	data() {
+		return {
+			swiperOptionTop: {
+				spaceBetween: 10,
+				loop: true,
+				loopedSlides: 5, //looped slides should be the same
+				navigation: {
+					nextEl: ".swiper-button-next",
+					prevEl: ".swiper-button-prev",
+				},
+			},
+			swiperOptionThumbs: {
+				spaceBetween: 10,
+				slidesPerView: 4,
+				touchRatio: 0.2,
+				loop: true,
+				loopedSlides: 5, //looped slides should be the same
+				slideToClickedSlide: true,
+			},
+			slides: [
+				"https://res.cloudinary.com/dgkw9qspp/image/upload/v1571562370/client-img-10_1_asbbfb.webp",
+				// "https://res.cloudinary.com/dgkw9qspp/image/upload/v1571562312/client-img-9_1_magmx4.webp",
+				// "https://res.cloudinary.com/dgkw9qspp/image/upload/v1571562258/client-img-8_1_qoxzwt.webp",
+				"https://res.cloudinary.com/dgkw9qspp/image/upload/v1571561966/client-img-3_1_qhsxmz.webp",
+				"https://res.cloudinary.com/dgkw9qspp/image/upload/v1571562211/client-img-7_1_ik7x2x.webp",
+				"https://res.cloudinary.com/dgkw9qspp/image/upload/v1571562093/client-img-5_1_qimmco.webp",
+				"https://res.cloudinary.com/dgkw9qspp/image/upload/v1571561733/client-img-1_1_pk3ip6.webp",
+				"https://res.cloudinary.com/dgkw9qspp/image/upload/v1571561885/client-img-2_1_qencbx.webp",
+				// "https://res.cloudinary.com/dgkw9qspp/image/upload/v1571562031/client-img-4_1_k0zfzc.webp",
+				"https://res.cloudinary.com/dgkw9qspp/image/upload/v1571562143/client-img-6_1_idhztt.webp",
+			],
+		}
+	},
+	components: {
+		swiper,
+		swiperSlide,
+	},
+	mounted() {
+		this.$nextTick(() => {
+			const swiperTop = this.$refs.swiperTop.swiper
+			const swiperThumbs = this.$refs.swiperThumbs.swiper
+			swiperTop.controller.control = swiperThumbs
+			swiperThumbs.controller.control = swiperTop
+		})
+	},
+}
 </script>
-<style>
-.main {
-  margin: 6px;
+<style lang="scss">
+.swiper-slide {
+	background-size: cover;
+	overflow: hidden;
+	background-position: center;
+}
+.gallery-top {
+	height: 75vh;
+	width: 100%;
+}
+.gallery-thumbs {
+	height: 20vh !important;
+	box-sizing: border-box;
+	padding: 6px 0;
+}
+.gallery-thumbs .swiper-slide {
+	width: 25%;
+	height: 100%;
+}
+.gallery-thumbs .swiper-slide-active {
+	opacity: 0.6;
+}
+/* .main {
+	margin: 6px;
 }
 
 .thumbnails {
-  margin: 0 4px;
-  width: calc(100% - 10px);
+	margin: 0 4px;
+	width: calc(100% - 10px);
 }
 
 .agile__nav-button {
-  background: transparent;
-  border: none;
-  color: #ccc;
-  cursor: pointer;
-  font-size: 24px;
-  transition-duration: 0.3s;
+	background: transparent;
+	border: none;
+	color: #ccc;
+	cursor: pointer;
+	font-size: 24px;
+	transition-duration: 0.3s;
 }
 .thumbnails .agile__nav-button {
-  position: absolute;
-  top: 50%;
-  -webkit-transform: translateY(-50%);
-  transform: translateY(-50%);
+	position: absolute;
+	top: 50%;
+	-webkit-transform: translateY(-50%);
+	transform: translateY(-50%);
 }
 .thumbnails .agile__nav-button--prev {
-  left: 25px;
+	left: 25px;
 }
 .thumbnails .agile__nav-button--next {
-  right: 25px;
+	right: 25px;
 }
 
 .slide {
-  align-items: center;
-  box-sizing: border-box;
-  color: #fff;
-  display: flex;
-  height: 76vh;
-  justify-content: center;
+	align-items: center;
+	box-sizing: border-box;
+	color: #fff;
+	display: flex;
+	height: 76vh;
+	justify-content: center;
 }
 .slide--thumbniail {
-  cursor: pointer;
-  height: 100px;
-  padding: 0 2px;
-  transition: opacity 0.3s;
+	cursor: pointer;
+	height: 100px;
+	padding: 0 2px;
+	transition: opacity 0.3s;
 }
 .slide--thumbniail:hover {
-  opacity: 0.75;
+	opacity: 0.75;
 }
 .slide img {
-  height: 100%;
-  -o-object-fit: cover;
-  object-fit: cover;
-  -o-object-position: center;
-  object-position: center;
-  width: 100%;
-  border-radius: 6px;
-}
+	height: 100%;
+	-o-object-fit: cover;
+	object-fit: cover;
+	-o-object-position: center;
+	object-position: center;
+	width: 100%;
+	border-radius: 6px;
+} */
 </style>
--- src/main.js
+++ src/main.js
@@ -1,24 +1,22 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import "@/assets/css/tailwind.css";
-import VueAgile from "vue-agile";
-import Lightbox from "vue-my-photos";
-import "../compiled-icons";
-import VueSVGIcon from "vue-svgicon";
+import Vue from "vue"
+import App from "./App.vue"
+import router from "./router"
+import "@/assets/css/tailwind.css"
+import Lightbox from "vue-my-photos"
+import "../compiled-icons"
+import VueSVGIcon from "vue-svgicon"
 
 Vue.use(VueSVGIcon, {
-  tagName: "icon",
-  isStroke: true,
-  defaultWidth: "42px",
-  defaultHeight: "42px",
-  defaultStrokeWidth: "1.2"
-});
-Vue.component("lightbox", Lightbox);
-Vue.use(VueAgile);
-Vue.config.productionTip = false;
+	tagName: "icon",
+	isStroke: true,
+	defaultWidth: "42px",
+	defaultHeight: "42px",
+	defaultStrokeWidth: "1.2",
+})
+Vue.component("lightbox", Lightbox)
+Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount("#app");
+	router,
+	render: h => h(App),
+}).$mount("#app")
